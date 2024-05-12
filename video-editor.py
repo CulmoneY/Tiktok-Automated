6bca695dcab7c071aedfd53d044d5b8324941051
Omid Hemmati <129822623+hemmatio@users.noreply.github.com>
2024-05-12 16:46:45 -0400

    uploaded video

--- video-editor.py
+++ video-editor.py
@@ -6,5 +6,12 @@ each lasting 61 seconds. Each clip is then enhanced with subtitles and a part nu
 A watermark is also added to each clip. The processed clips are saved as mp4 files in a specified folder.
 
 """
+from typing import IO
 from moviepy.editor import *
-def movie_spilter(video:)
+def movie_spilter(filename: str):
+    """Spilts the given video into multiple 61 second part. Each clip is enchanced with a part number, and save as a temp. mp4 file
+
+        Args:
+        video: The path fto the MP4 video file
+        """
+        video = VideoFileClip(filename)
