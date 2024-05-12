d3612bceaeb33d5a7a067b297a15ba8026459de4
Omid Hemmati <129822623+hemmatio@users.noreply.github.com>
2024-05-12 17:07:29 -0400

    changed video to mp4
    Co-Authored-By: CulmoneY <154041314+CulmoneY@users.noreply.github.com>

--- video-editor.py
+++ video-editor.py
@@ -8,10 +8,22 @@ A watermark is also added to each clip. The processed clips are saved as mp4 fil
 """
 from typing import IO
 from moviepy.editor import *
+
 def movie_spilter(filename: str):
     """Spilts the given video into multiple 61 second part. Each clip is enchanced with a part number, and save as a temp. mp4 file
 
-        Args:
-        video: The path fto the MP4 video file
-        """
-        video = VideoFileClip(filename)
+    Args:
+    video: The path fto the MP4 video file
+    """
+    video = VideoFileClip(filename)
+    num_parts = video.duration() // 61
+    for part in range(1, num_parts + 1):
+        # Make the part text
+        txt_clip = (TextClip(f"Part {part}", fontsize = 40, color = 'white', bg_color = 'red')
+                    .set_position('North').set_duration(10))
+        video_part = video.subclip(61 * (part - 1), 61 * part)
+        result = CompositeVideoClip([video_part, txt_clip])
+
+        video.ipython_display()
+
+
