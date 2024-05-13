fc7b667cbc33246c8f7541e0ca00d135af91d0fd
Omid Hemmati <129822623+hemmatio@users.noreply.github.com>
2024-05-12 20:52:40 -0400

    made readme
    Co-Authored-By: CulmoneY <154041314+CulmoneY@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Tiktok-Automated
+
+# Tiktok Automated
+
+Have you ever seen a movie split into 50 parts on TikTok and thought "holy shit, that's a lot of views!". No? Just me?
+
+#### Tiktok Automated automatically splits a movie into parts, subtitles them, then uploads them to TikTok.
+
+
+
+
+## Authors
+
+- [@CulmoneY](https://github.com/CulmoneY)
+- [@hemmatio](https://github.com/hemmatio)
+
+
+
+
+## Required Libraries/Software
+- subtitlegenerator
+- moviepy
+    - [imagemagick](https://imagemagick.org/index.php)
+- ffmpeg
+- whisper
+imagemagick should be installed separately (not using pip). After installation, be sure to correctly set the path of 'IMAGEMAGICK_BINARY' under external_libraries/site-packages/moviepy/config_defaults.py to 'C:\Program Files\ImageMagick-versionnumber\magick.exe', or wherever your installation was made to.
--- videoeditor.py
+++ videoeditor.py
@@ -21,30 +21,34 @@ def movie_splitter(filename: str):
     print("Video Duration: ", video.duration)
     num_parts = math.ceil(video.duration / 61)
     print(num_parts)
+
+    # Get the subtitles
+    video = CompositeVideoClip([video] + subtitles(run(filename)))
+
     for part in range(1, num_parts):
         # Make the part text
-        txt_clip = (TextClip(f"Part {part}", fontsize=20, color='white', bg_color='red')
-                    .set_position('center', 'top').set_duration(10))
-
+        txt_clip = (TextClip(f"Part {part}", fontsize=20, color='black', bg_color='white')
+                    .set_position(('center', 0.1), relative=True).set_duration(10))
         video_part = video.subclip(61 * (part - 1), 61 * part)
         result = CompositeVideoClip([video_part, txt_clip])
-
         result.write_videofile(f"part_{part}.mp4")
 
     # for the remaining portion of the video
     final_duration = min(10, video.duration - 61 * (num_parts - 1))
-    txt_clip = (TextClip(f"Final", fontsize=20, color='white', bg_color='red')
-                .set_position('center', 'top').set_duration(final_duration)) # for subtitles, this should be a list of TextClips
+    txt_clip = (TextClip(f"Final", fontsize=20, color='black', bg_color='white')
+                .set_position(('center', 0.1), relative=True).set_duration(final_duration)) # for subtitles, this should be a list of TextClips
     video_part = video.subclip(61 * (num_parts - 1))
+
     result = CompositeVideoClip([video_part, txt_clip])
-    result.set_duration(video.duration - 61 * (num_parts - 1)).write_videofile(f"part_{num_parts}.mp4")
+    result.set_duration(video_part.duration).write_videofile(f"part_{num_parts}.mp4")
+
 
 def subtitles(captions: list) -> list:
     """Processes each caption from captions and converts them into a txt_clip"""
     text_clips = []
     for caption in captions:
         start_time, end_time, text = caption
-        txt_clip = TextClip(text, fontsize=20, color='white')
-        txt_clip.set_start(start_time).set_duration(end_time - start_time).set_position('center', 'bottom')
+        txt_clip = TextClip(text, fontsize=24, color='white')
+        txt_clip = txt_clip.set_start(start_time).set_duration(end_time - start_time).set_position('center')
         text_clips.append(txt_clip)
     return text_clips
